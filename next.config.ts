fc5970c5653aa1b68ff788d94830b7918b5d105d
Almas M <kramal888@gmail.com>
2025-12-08 08:10:59 +0100

    feat: v1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        formats: ["image/webp"], // only WebP output
+    },
 };
 
 export default nextConfig;
